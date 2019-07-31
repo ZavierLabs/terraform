488ea91f76d6ca1f243745a0c7a0f528e26c8899
Javier <javier.nunez@naranja.com>
2019-07-31 12:14:42 -0300

    New commit

---
+++ devops/modules/Networking/main.tf
@@ -0,0 +1,32 @@
+module "main" {
+  module "aws_eip" {
+    source = "../../resources/aws_eip"
+  }
+
+  module "aws_instance" {
+    source = "../../resources/aws_instance"
+  }
+
+  module "aws_internet_gateway" {
+    source = "../../resources/aws_internet_gateway"
+  }
+  module "aws_nat_gateway" {
+    source = "../../resources/aws_nat_gateway"
+  }
+  module "aws_route" {
+    source = "../../resources/aws_route"
+  }
+  module "aws_route_table" {
+    source = "../../resources/aws_route_table"
+  }
+  module "aws_route_table_association" {
+    source = "../../resources/aws_route_table_association"
+  }
+  module "aws_subnet" {
+    source = "../../resources/aws_subnet"
+  }
+  module "aws_vpc" {
+    source = "../../resources/aws_vpc"
+  }
+}
+
